fe6ce83a449310ae13fa2a3fa7e72c25c644b3c1
Deprecatism <70801324+Deprecatism@users.noreply.github.com>
2026-04-03 08:40:55 +0530

    Add cunnyx to fxtwitter replace (get fucked hazye)

--- extensions/utility/__init__.py
+++ extensions/utility/__init__.py
@@ -22,7 +22,7 @@ if TYPE_CHECKING:
 SHAMIKO_SERVER_ID = 682869291997331466
 SHAMIKO_CHAT_CHANNEL_ID = 705071817081094246
 
-FXTWITTER_MATCH = r'https?:\/\/(?:x|twitter)\.com\/(?:\w+)\/status\/(\d+)(?:\S+)?'
+FXTWITTER_MATCH = r'https?:\/\/(?:x|twitter|cunnyx)\.com\/(?:\w+)\/status\/(\d+)(?:\S+)?'
 FXTWITTER_REPLACE = r'https://fxtwitter.com/status/\g<1>'
 
 SKPORT_REMINDER_ROLE = 1479873899159093420
